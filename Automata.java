1353137a9a06677800fcf01d2e917c370bc0785c
yisseddenisse <74641431+yisseddenisse@users.noreply.github.com>
2020-11-18 14:58:47 -0600

    Update Automata.java

--- Automata.java
+++ Automata.java
@@ -11,14 +11,32 @@ String  alphabet[]=new String[2];//debido a que solo se permitiran dos caractere
 //buffer readaer
 
 public void askTupla(){
+  try{
   String edos,in,fin;
     edos = JOptionPane.showInputDialog("Introduzca el número de estados que se desea:");
-    in= JOptionPane.showInputDialog("Introduzca el estado que deseas que sea el edo inicial");
-    fin=JOptionPane.showInputDialog("Introduzca el edo que deasea que sea el final");
-
     x=Integer.parseInt(edos);
+    in= JOptionPane.showInputDialog("Introduzca el estado que deseas que sea el edo inicial");
     initial=Integer.parseInt(in);
-    fin=Integer.parseInt(fin);
+    fin=JOptionPane.showInputDialog("Introduzca el número de estados finales");
+    finalesta=new char[Integer.parseInt(fin)];
+    for(int i=0; i<finalesta.length; i++)
+        {
+            String f=JOptionPane.showInputDialog("Estado Final "+i+": ");
+            fi=fi+f;
+        }
+    finalesta=fin.toCharArray();
+        for(int i=0; i<finalesta.length; i++)
+        {
+            JOptionPane.showMessageDialog(null,"Caracter "+finalesta[i]+" ingresado");
+        }
+      }
+     catch(Exception edos)
+     {
+            JOptionPane.showMessageDialog(null,"Error "+edos); 
+            x=0;
+            finalesta=new char[0];
+            fi="";
+     }
 
 }
 
