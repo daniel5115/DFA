ad4e78082887620e2439c35810ac426a919b1609
daniel5115 <72767578+daniel5115@users.noreply.github.com>
2020-11-17 23:12:42 -0600

    Add files via upload

---
+++ Automata.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+
+public class Automata{
+
+int x;//numero de elfos que habra , se le preguntara en la quintupla
+State[] statesAll=new State[x];//maximo 5 caracteres
+int initial;
+int final;
+String  alphabet[]=new String[2];//debido a que solo se permitiran dos caracteres
+
+
+
+
+
+
+
+
+
+public static void(String args[] ){
+
+}
+
+
+}
---
+++ State.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+
+public class State{
+private int number;
+private boolean initial;
+private boolean isfinal;
+
+
+public State(){
+  number=0;
+  initial=false;
+  isfinal=false;
+
+
+}
+
+public int getNumber(){
+  return number;
+}
+
+public boolean getInitial(){
+  return initial;
+}
+
+public boolean getFinal(){
+  return isfinal;
+
+}
+
+public void setNumber(int n){
+  number=n;
+}
+
+public void setInitial(boolean a){
+  initial=a;
+
+}
+
+public void setFinal(boolean f){
+  isfinal=f;
+}
+
+}
